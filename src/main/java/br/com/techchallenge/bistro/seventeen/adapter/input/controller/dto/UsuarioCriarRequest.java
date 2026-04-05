faefd4cc92ef1dfc1a6a915065652acb7ddc4a03
Ricardo Aguirra <ramenendes@gmail.com>
2026-04-05 20:19:40 -0300

    Regra Endereco/Role - CriaUserRequest

--- src/main/java/br/com/techchallenge/bistro/seventeen/adapter/input/controller/dto/UsuarioCriarRequest.java
+++ src/main/java/br/com/techchallenge/bistro/seventeen/adapter/input/controller/dto/UsuarioCriarRequest.java
@@ -2,6 +2,7 @@ package br.com.techchallenge.bistro.seventeen.adapter.input.controller.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record UsuarioCriarRequest(
 
@@ -18,8 +19,11 @@ public record UsuarioCriarRequest(
         @NotBlank(message = "Senha obrigatória para realizar o cadastro")
         String senha,
 
+        @NotBlank(message = "Endereço é obrigatório para realizar do cadastro.")
         String endereco,
 
+        @NotBlank(message = "Role é obrigatória.")
+        @Pattern(regexp = "DONO_RESTAURANTE|CLIENTE", message = "Role deve ser DONO_RESTAURANTE ou CLIENTE.")
         String role
 
 ) {
