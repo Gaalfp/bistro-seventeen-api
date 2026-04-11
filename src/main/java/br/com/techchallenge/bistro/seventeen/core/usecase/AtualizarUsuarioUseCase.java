f10212d403b44d3d07623ac6a137dd2ff1e33f57
Jeniffer Bandeira <jeniffer.bandeira@gmail.com>
2026-04-11 17:54:44 -0300

    feat: implementa validacoes de unicidade e inatividade na atualizacao de usuario

--- src/main/java/br/com/techchallenge/bistro/seventeen/core/usecase/AtualizarUsuarioUseCase.java
+++ src/main/java/br/com/techchallenge/bistro/seventeen/core/usecase/AtualizarUsuarioUseCase.java
@@ -1,10 +1,13 @@
 package br.com.techchallenge.bistro.seventeen.core.usecase;
 
+import br.com.techchallenge.bistro.seventeen.core.exception.RecursoEmConflitoException;
 import br.com.techchallenge.bistro.seventeen.core.exception.RecursoJaExisteException;
 import br.com.techchallenge.bistro.seventeen.core.exception.UsuarioNaoEncontradoException;
 import br.com.techchallenge.bistro.seventeen.core.model.Usuario;
 import br.com.techchallenge.bistro.seventeen.port.input.AtualizarUsuarioInputPort;
 import br.com.techchallenge.bistro.seventeen.port.output.UsuarioRepositoryOutputPort;
+
+import java.time.LocalDateTime;
 
 public class AtualizarUsuarioUseCase implements AtualizarUsuarioInputPort {
 
@@ -16,10 +19,14 @@ public class AtualizarUsuarioUseCase implements AtualizarUsuarioInputPort {
 
     @Override
     public Usuario atualizarUsuario(Usuario usuario) {
-         Usuario usuarioExistente = usuarioRepositoryOutputPort.buscarPorId(usuario.getId())
+        Usuario usuarioExistente = usuarioRepositoryOutputPort.buscarPorId(usuario.getId())
                 .orElseThrow(() -> new UsuarioNaoEncontradoException(
                         "Usuário com ID " + usuario.getId() + " não encontrado"
                 ));
+
+        if (!usuarioExistente.isAtivo()) {
+            throw new RecursoEmConflitoException("Não é possível atualizar os dados de um usuário inativo.");
+        }
 
         if (!usuarioExistente.getEmail().equals(usuario.getEmail())) {
             usuarioRepositoryOutputPort.buscarPorEmail(usuario.getEmail())
@@ -37,6 +44,10 @@ public class AtualizarUsuarioUseCase implements AtualizarUsuarioInputPort {
 
         usuario.setCpf(usuarioExistente.getCpf());
         usuario.setSenhaHash(usuarioExistente.getSenhaHash());
+        usuario.setAtivo(usuarioExistente.isAtivo());
+
+        usuario.setDataUltimaAlteracao(LocalDateTime.now());
+
         return usuarioRepositoryOutputPort.salvar(usuario);
     }
 }
